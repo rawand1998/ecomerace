18066705de5d3fffbb2552111fc21410b059668e
rawand1998 <rawandgaradh@gmail.com>
2022-07-01 15:39:53 +0300

    sub header descritipn

--- src/Components/SubHeader.js
+++ src/Components/SubHeader.js
@@ -2,7 +2,14 @@ import React from 'react'
 
 function SubHeader() {
   return (
-    <div>SubHeader</div>
+    <div className="subheader-conatiner">
+        <div className="subheader-description">
+            <h1>Experts<br/> with Mega <br/> vision.</h1>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br />Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+            <button>Explore Now</button>
+        </div>
+        <div className="subheader-image"></div>
+    </div>
   )
 }
 
