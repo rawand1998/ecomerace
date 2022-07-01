2add3809b7aabc7bef4c23ed6215b563a03ccc0a
rawand1998 <rawandgaradh@gmail.com>
2022-07-01 17:37:02 +0300

    product component style

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import Header from './Components/Header';
 import About from './Components/About';
+import Product from './Components/Product';
 function App() {
   return (
     <div className="App">
     <Header />
     <About />
+    <Product />
     </div>
   );
 }
---
+++ src/Components/Product.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Product() {
+  return (
+    <div className="product-container">
+        <div className="product-description">
+            <span>PRODUCTS</span>
+            <p className="product-title">Shopping the way<br /> you like it.</p>
+            <p className="product-title-description">Lorem Ipsum is simply dummy text of the printing and typesetting industry. <br />Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,<br/>Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+            <p className="product-explore">Explore Now</p>
+        </div>
+        <div className="product-image"></div>
+    </div>
+  )
+}
+
+export default Product
