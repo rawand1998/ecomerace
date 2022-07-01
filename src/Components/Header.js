6703a55f0487ae4430bd23256812fe282f60f3b3
rawand1998 <rawandgaradh@gmail.com>
2022-07-01 10:48:41 +0300

    create header component

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from './NavBar'
+function Header() {
+  return (
+    <div className="header-container">
+   <NavBar />
+   </div>
+  )
+}
+
+export default Header
