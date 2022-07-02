a616952ad9b69774972da850191e58b5e4007799
rawand1998 <rawandgaradh@gmail.com>
2022-07-02 08:43:37 +0300

    add router to products in navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { FaSearch } from 'react-icons/fa';
 import { FaShoppingCart } from 'react-icons/fa';
+import {  Link } from "react-router-dom";
 function NavBar() {
   return (
     <div className="nav-container">
         <div className="logo"><h2>Bensike</h2></div>
         <div className="list">
             <a>HOME</a>
-            <a>PRODUCTS</a>
+            <Link to="/products">PRODUCTS</Link>
             <a>ABOUT</a>
             <a>Support</a>
         </div>
