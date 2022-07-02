271b58d705cfe8383a85da3a524f8e9714186573
rawand1998 <rawandgaradh@gmail.com>
2022-07-02 10:04:29 +0300

    product details component

--- src/Components/ProductPageDetails.js
+++ src/Components/ProductPageDetails.js
@@ -1,9 +1,32 @@
-import React from 'react'
-
+import React, { useEffect,useState } from "react";
+import { useParams } from "react-router-dom";
+// import { db } from "../firebase";
+import firebase from "../firebase";
+import { getDocs, collection, query,doc } from "firebase/firestore";
 function ProductPageDetails() {
-  return (
-    <div>ProductPageDetails</div>
-  )
+  const db = firebase.firestore();
+  const param = useParams();
+ 
+  const [product,setProduct]=useState([]);
+  useEffect(() => {
+
+    const getproductDetails = async () => {
+        db.collection('products').doc(param.id).get().then((product) => {
+            setProduct(product.data())
+        })
+ 
+    };
+    getproductDetails();
+  });
+  return <div>
+
+          <img src={product.productImg} />
+            <h3>{product.productName}</h3>
+            <p>{product.description}</p>
+            <span>{product.price}</span>
+            <span>{product.category}</span>
+            <span>{product.amount}</span>
+    </div>;
 }
 
-export default ProductPageDetails
+export default ProductPageDetails;
