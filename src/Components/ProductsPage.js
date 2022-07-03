d50e940608c9d2f35d2443e88f83fb9555bc8f46
rawand1998 <rawandgaradh@gmail.com>
2022-07-03 09:32:45 +0300

    edit filter

--- src/Components/ProductsPage.js
+++ src/Components/ProductsPage.js
@@ -6,37 +6,33 @@ import { useNavigate } from "react-router-dom";
 import Header from "./Header";
 function ProductsPage() {
   const [productsList, setProductsList] = useState([]);
-  const [selected,setSelected] = useState('');
+  const [selected, setSelected] = useState("all");
   const db = firebase.firestore();
   const productRef = collection(db, "products");
   let navigate = useNavigate();
   useEffect(() => {
-    const getProducts =  () => {
-      console.log(selected,"===========")
-     const data = db.collection('products').where('category', '==', selected).get().then((res)=>{
-      console.log("res",res)
-      setProductsList(
-        res.docs.map((item) => ({ ...item.data(), id: item.id }))
-      );
-    
-     });
-     console.log(data);
-
-      
-      
+    const getProducts = () => {
+      db.collection("products")
+        .where("category", "==", selected)
+        .get()
+        .then((res) => {
+          setProductsList(
+            res.docs.map((item) => ({ ...item.data(), id: item.id }))
+          );
+        });
     };
+
     getProducts();
   }, [selected]);
 
   const addCart = (id) => {
     navigate(`/products/${id}`);
   };
-  
- 
 
   return (
     <div className="products-firestore">
-      <select  onChange={(e)=>setSelected(e.target.value)} value={selected}>
+      <select onChange={(e) => setSelected(e.target.value)} value={selected}>
+        <option value="all">all</option>
         <option value="shoe">shoe</option>
         <option value="bag">bag</option>
         <option value="woman">woman</option>
