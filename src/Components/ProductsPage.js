4f1b694c948f404de1cbfb68ee27534e17a881f7
rawand1998 <rawandgaradh@gmail.com>
2022-07-03 11:47:44 +0300

    create other syb navbar

--- src/Components/ProductsPage.js
+++ src/Components/ProductsPage.js
@@ -32,35 +32,37 @@ function ProductsPage() {
 
   return (
     <div>
-        <NavBar />
+      <NavBar />
       <div className="sub-navabr">
-     <a>new in</a> 
-<a>summer fits</a>
-<a>going out</a>
-<a>dresses</a>
-<a>occasion</a>
-<a>tops</a>
-<a>trousers</a>
-<a>clothing</a>
-<a>
-shop by body fit</a>
-<a>shoes</a>
-<a>mg kids</a>
-<a>
-homeware</a>
-<a>sale</a>
-
-
-
-
-
-
-
-
-
+        <a>new in</a>
+        <a>summer fits</a>
+        <a>going out</a>
+        <a>dresses</a>
+        <a>occasion</a>
+        <a>tops</a>
+        <a>trousers</a>
+        <a>clothing</a>
+        <a>shop by body fit</a>
+        <a>shoes</a>
+        <a>mg kids</a>
+        <a>homeware</a>
+        <a>sale</a>
+      </div>
+      <div className="pink-navbar">
+        <div className="pink">
+          <h3>SUMMAR IS HERE!</h3>
+          <p>Shop summar outfits</p>
+        </div>
+        <div className="pink2">
+        <h3>50% FOR EVERYTHING!CODE:TAVER</h3>
+          <p>limit time only</p>
+        </div>
+        <div className="pink3">
+        <h3>EVERY THING MUST GO!</h3>
+          <p>secure the bag now</p>
+        </div>
       </div>
       <select onChange={(e) => setSelected(e.target.value)} value={selected}>
-        
         <option value="clothes">clothes</option>
         <option value="bag">bag</option>
         <option value="shoe">shoe</option>
