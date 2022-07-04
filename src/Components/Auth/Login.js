168a2b0b469d2e66e90cbc2918c36f299090d6a2
rawand1998 <rawandgaradh@gmail.com>
2022-07-04 12:28:35 +0300

    login form

--- src/Components/Auth/Login.js
+++ src/Components/Auth/Login.js
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useState } from "react";
+import firebase from "../../firebase";
+import { login } from "../../hooks";
+import { useNavigate } from "react-router-dom";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  let navigate = useNavigate();
+  const Login = (e) => {
+    e.preventDefault();
 
-function Login() {
+    login(email, password);
+    navigate('/')
+  };
   return (
-    <div>Login</div>
-  )
-}
+    <div>
+      <form>
+        <label>Email</label>
+        <input
+          type="email"
+          placeholder="Email Address"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <label>Password</label>
+        <input
+          type="password"
+          placeholder="Email Address"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button onClick={Login}>Send</button>
+      </form>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
