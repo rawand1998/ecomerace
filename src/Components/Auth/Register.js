12793b4899ce2442d0accd9023277e713cde6c6d
rawand1998 <rawandgaradh@gmail.com>
2022-07-04 12:21:09 +0300

    register form

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState ,useContext} from "react";
+import firebase from "../../firebase";
+import { register } from "../../hooks";
+
 
 function Register() {
-  return (
-    <div>Register</div>
-  )
-}
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [name, setName] = useState("");
+    const Register = (e) => {
+        e.preventDefault();
+    
+        register(name, email, password);
+      };
+      return (
+        <div>
+          <form>
+            <label>Name</label>
+            <input
+              value={name}
+              type="text"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <label>Email</label>
+            <input
+              type="email"
+              placeholder="Email Address"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              placeholder="Email Address"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <button onClick={Register}>Send</button>
+          </form>
+        </div>
+      );
+    };
 
 export default Register
