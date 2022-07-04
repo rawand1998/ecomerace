c9885300fb30b3bf9ea37532b34ce68309c99c93
rawand1998 <rawandgaradh@gmail.com>
2022-07-04 16:56:26 +0300

    handle error in details page

---
+++ src/Components/Error.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Error({Massage}) {
+  return (
+    <div>
+        <p>{Massage}</p>
+    </div>
+  )
+}
+
+export default Error
--- src/Components/ProductPageDetails.js
+++ src/Components/ProductPageDetails.js
@@ -3,18 +3,20 @@ import { useParams } from "react-router-dom";
 import { FaPlus } from "react-icons/fa";
 import { FaMinus } from "react-icons/fa";
 import firebase from "../firebase";
-import { getDocs, collection, query, doc,addDoc } from "firebase/firestore";
+import { getDocs, collection, query, doc, addDoc } from "firebase/firestore";
 import NavBar from "./NavBar";
 import SubNavBar from "./SubNavBar";
 import Footer from "./Footer";
 import { auth } from "../firebase";
-import {checkLogin} from '../hooks'
+import { checkLogin } from "../hooks";
+import Error from "./Error";
 function ProductPageDetails() {
   const db = firebase.firestore();
   const param = useParams();
 
   const [product, setProduct] = useState([]);
- 
+  const [msg, setMsg] = useState("");
+
   useEffect(() => {
     const getproductDetails = async () => {
       db.collection("products")
@@ -28,61 +30,70 @@ function ProductPageDetails() {
   });
 
   const increaAmount = (productAmount, price) => {
-  
     db.collection("products")
       .doc(param.id)
-      .update({ amount: productAmount +1, price:price * (++productAmount)})
-    }
-    const shoppineCart = (productName,productAmount,productPrice)=>{
-      auth.onAuthStateChanged((user) => {
-        if (user) {
-         
-         db.collection("cart").doc(param.id).set({
-            productName:productName,
-            amount:productAmount,
-            price:productPrice
-        })
-       
-        }else{
-          console.log("erro")
-        }
-      });
-      
-    }
-    const decreaAmount =(productAmount,price)=>{
-      db.collection("products")
+      .update({ amount: productAmount + 1, price: price * ++productAmount });
+  };
+  const shoppineCart = (productName, productAmount, productPrice) => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        db.collection("cart").doc(param.id).set({
+          productName: productName,
+          amount: productAmount,
+          price: productPrice,
+        });
+      } else {
+        setMsg("You Should To Be Login!");
+        console.log(msg)
+      }
+    });
+  };
+  const decreaAmount = (productAmount, price) => {
+    db.collection("products")
       .doc(param.id)
-      .update({ amount: productAmount -1, price:price * (--productAmount)})
-    }
+      .update({ amount: productAmount - 1, price: price * --productAmount });
+  };
   return (
     <div className="details">
       <NavBar />
       {/* <SubNavBar /> */}
-    <div className="product-details">
-      <img src={product.productImg} />
-      <div className="product-details-description">
-        <span className="productName">product Name</span>
-      <h3>{product.prorductName}</h3>
-      <span className="productDescription">product description</span>
-      <p>{product.description}</p>
-      <span className="price">price: {product.price}</span>
-      <span className="price">Category: {product.category}</span>
-      
-      <p className="amount">
-        Amount: {" "}
-        {product.amount}
-        <div className="amount-btns">
-        <FaPlus onClick={() => increaAmount(product.amount,product.price)} className="amount-inc"/> 
-       <FaMinus onClick={() => decreaAmount(product.amount,product.price)} className="amount-dec"/>
-       </div>
-      </p>
-     
-      <button className="btn-details" onClick={()=>shoppineCart(product.prorductName,product.amount, product.price)}>Add to cart</button>
+      <div className="product-details">
+        <img src={product.productImg} />
+        <div className="product-details-description">
+          <span className="productName">product Name</span>
+          <h3>{product.prorductName}</h3>
+          <span className="productDescription">product description</span>
+          <p>{product.description}</p>
+          <span className="price">price: {product.price}</span>
+          <span className="price">Category: {product.category}</span>
+
+          <p className="amount">
+            Amount: {product.amount}
+            <div className="amount-btns">
+              <FaPlus
+                onClick={() => increaAmount(product.amount, product.price)}
+                className="amount-inc"
+              />
+              <FaMinus
+                onClick={() => decreaAmount(product.amount, product.price)}
+                className="amount-dec"
+              />
+            </div>
+          </p>
+
+          <button
+            className="btn-details"
+            onClick={() =>
+              shoppineCart(product.prorductName, product.amount, product.price)
+            }
+          >
+            Add to cart
+          </button>
+          <p className="err-msg">{msg}</p>
+        </div>
+       
       </div>
-    
-     
-    </div>
-  <Footer />
+      <Footer />
     </div>
   );
 }
