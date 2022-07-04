2559f8019011f6cda0391dd896fd15229fb2b0fc
rawand1998 <rawandgaradh@gmail.com>
2022-07-04 12:12:31 +0300

    create hooks file

---
+++ src/hooks/index.js
@@ -0,0 +1,22 @@
+import firebase from "../firebase";
+import { auth } from "../firebase";
+
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+import { addDoc, collection, query } from "@firebase/firestore";
+import { db } from "../firebase";
+export const register = (name, email, password) => {
+  createUserWithEmailAndPassword(auth, email, password).then((res) => {
+    let user = res.user;
+    console.log(user.uid);
+    addDoc(collection(db, "name"), {
+      uid: user.uid,
+      name,
+      email,
+    });
+  });
+};
+export const login =async (email, password) => {
+     await signInWithEmailAndPassword(auth,email, password);}
