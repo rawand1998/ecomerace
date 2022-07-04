c8227b130d197c728806e09740b3187e95e3998a
rawand1998 <rawandgaradh@gmail.com>
2022-07-04 12:30:28 +0300

    login route in navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -19,7 +19,7 @@ function NavBar() {
         <div className="list">
         <Link to="/" className="product-navbar">HOME</Link>
             <Link to="/products" className="product-navbar">PRODUCTS</Link>
-            <a>Login</a>
+            <Link to="/login" className="product-navbar">LOGIN</Link>
             <a>Support</a>
         </div>
         <div className="navBar-icon">
