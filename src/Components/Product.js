8cfeb115e2d4f373697c439063b3aa7d3bbe6286
rawand1998 <rawandgaradh@gmail.com>
2022-07-05 18:27:28 +0300

    edit some style

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -8,7 +8,11 @@ function Product() {
             <p className="product-title">Everybody is different, which is why we offer styles for every body.<br/> Laborum fuga incidunt laboriosam <br/>voluptas iure, delectus dignissimos facilis neque nulla earum..</p>
            
         </div>
-        <div className="product-image"></div>
+        <div className="product-image">
+          <img src="https://preview.colorlib.com/theme/capitalshop/assets/img/gallery/xfounder-img.png.pagespeed.ic.4PZGL9Sx_O.webp"/>
+          <span>Petey Cruiser</span><br />
+        
+        </div>
     </div>
   )
 }
