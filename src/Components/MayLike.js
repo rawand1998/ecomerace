05fa3f9a5a78b1f1847e7905593913a8c2b7e6d0
rawand1998 <rawandgaradh@gmail.com>
2022-07-05 12:40:41 +0300

    testimonal change style

---
+++ src/Components/MayLike.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+function MayLike() {
+  return (
+    <div className="mayLike-container">
+    <div className="like-nav">
+        <h2>May Like</h2>
+        
+    </div>
+
+    <div className="trend-img">
+       
+        <div className="trend-img1">
+        <div>
+            <img src="https://preview.colorlib.com/theme/capitalshop/assets/img/gallery/xlatest6.jpg.pagespeed.ic.Gs5H8uF_0V.webp"/>
+        </div>
+            <h3>Cashmere Tank + Bag</h3>
+            <span>852$</span>
+        </div>
+        <div className="trend-img1">
+        <div>
+            <img src="https://preview.colorlib.com/theme/capitalshop/assets/img/gallery/xlatest5.jpg.pagespeed.ic.WuUZFTYF8g.webp"/>
+        </div>
+            <h3>Cashmere Tank + Bag</h3>
+            <span>852$</span>
+        </div>
+        <div className="trend-img1">
+        <div>
+            <img src="https://preview.colorlib.com/theme/capitalshop/assets/img/gallery/xlatest7.jpg.pagespeed.ic.hx4jI0dHK5.webp"/>
+        </div>
+            <h3>Cashmere Tank + Bag</h3>
+            <span>852$</span>
+        </div>
+        <div className="trend-img1">
+        <div>
+            <img src="https://preview.colorlib.com/theme/capitalshop/assets/img/gallery/xlatest8.jpg.pagespeed.ic.qoQf2-mhtF.webp"/>
+        </div>
+            <h3>Cashmere Tank + Bag</h3>
+            <span>852$</span>
+        </div>
+    </div>
+</div>
+  )
+}
+
+export default MayLike
---
+++ src/Components/TrendingProduct.js
@@ -0,0 +1,51 @@
+import React from 'react'
+
+function TrendingProduct() {
+  return (
+    <div className="trend-container">
+        <div className="trend-nav">
+            <h2>Trending This Week</h2>
+            <div className="trend-menu">
+                <a>Woman</a>
+                <a>Men</a>
+                <a>Baby</a>
+                <a>Fashion</a>
+            </div>
+        </div>
+
+        <div className="trend-img">
+           
+            <div className="trend-img1">
+            <div>
+                <img src="https://preview.colorlib.com/theme/capitalshop/assets/img/gallery/xlatest4.jpg.pagespeed.ic.Lrd0UW2ap9.webp"/>
+            </div>
+                <h3>Cashmere Tank + Bag</h3>
+                <span>852$</span>
+            </div>
+            <div className="trend-img1">
+            <div>
+                <img src="https://preview.colorlib.com/theme/capitalshop/assets/img/gallery/xlatest2.jpg.pagespeed.ic.wreJQIyLHL.webp"/>
+            </div>
+                <h3>Cashmere Tank + Bag</h3>
+                <span>852$</span>
+            </div>
+            <div className="trend-img1">
+            <div>
+                <img src="https://preview.colorlib.com/theme/capitalshop/assets/img/gallery/xlatest3.jpg.pagespeed.ic.weRwbGvwtF.webp"/>
+            </div>
+                <h3>Cashmere Tank + Bag</h3>
+                <span>852$</span>
+            </div>
+            <div className="trend-img1">
+            <div>
+                <img src="https://preview.colorlib.com/theme/capitalshop/assets/img/gallery/xlatest1.jpg.pagespeed.ic.9LywwGvNeR.webp"/>
+            </div>
+                <h3>Cashmere Tank + Bag</h3>
+                <span>852$</span>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default TrendingProduct
