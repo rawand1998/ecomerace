626528d44a34e1a3ae7ece2df0aa6058b7edd2f5
rawand1998 <rawandgaradh@gmail.com>
2022-07-05 17:56:29 +0300

    register page stye

--- src/Components/Auth/Register.js
+++ src/Components/Auth/Register.js
@@ -1,43 +1,59 @@
 import React, { useState ,useContext} from "react";
 import firebase from "../../firebase";
 import { register } from "../../hooks";
-
+import { useNavigate } from "react-router-dom";
 
 function Register() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [name, setName] = useState("");
+    let navigate = useNavigate();
     const Register = (e) => {
         e.preventDefault();
     
         register(name, email, password);
+        navigate('/')
       };
       return (
-        <div>
-          <form>
-            <label>Name</label>
-            <input
-              value={name}
-              type="text"
-              onChange={(e) => setName(e.target.value)}
-            />
-            <label>Email</label>
-            <input
-              type="email"
-              placeholder="Email Address"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label>Password</label>
-            <input
-              type="password"
-              placeholder="Email Address"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <button onClick={Register}>Send</button>
-          </form>
+       
+         <div className="login-pg">
+      <div className="login-form-area">
+        <div className="login-form">
+        <div className="login-heading">
+          <span>Login</span>
+          <p>Enter Login details to get access</p>
         </div>
+        <div className="login-box">
+        <div className="single-input-feilds">
+            <label for="">Username </label>
+            <input  value={name}  placeholder="Email Address"
+          onChange={(e) => setName(e.target.value)} />
+          </div>
+          <div className="single-input-feilds">
+            <label for="">Email Address</label>
+            <input  value={email}  placeholder="Email Address"
+          onChange={(e) => setEmail(e.target.value)} />
+          </div>
+          <div  className="single-input-feilds">
+          <label>Password</label>
+        <input
+          type="password"
+          placeholder="Email Address"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+          </div>
+         
+        </div>
+        <div className="login-footer">
+          {/* <p> Don’t have an account? <Link to="/register">Sign Up</Link></p> */}
+          <button className="submit-btn3" onClick={Register}>Register </button>
+        </div>
+      </div>
+      </div>
+
+    
+    </div>
       );
     };
 
