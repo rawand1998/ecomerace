c0744c47f9ed297168d39fc6206223e60e701bef
rawand1998 <rawandgaradh@gmail.com>
2022-07-12 09:09:37 +0300

    create product detauls folder

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 import ProductsPage from './Components/ProductsPage';
 import Home from './Components/Home';
-import ProductPageDetails from './Components/ProductPageDetails';
+import ProductPageDetails from './Components/ProductPageDetails/ProductPageDetails';
 import ShoppingCartPgae from './Components/ShoppingCartPgae';
 import {BrowserRouter as Router , Routes,Route,Link} from 'react-router-dom'
 import Header from './Components/Header';
